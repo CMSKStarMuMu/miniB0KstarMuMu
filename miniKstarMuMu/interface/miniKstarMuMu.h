f38d8086d3fdfde68e4c62e4c954bc383fb9092c
sara <sara.fiorendi@cern.ch>
2018-05-30 15:03:19 +0200

    add gen part + other improvements

--- miniKstarMuMu/interface/miniKstarMuMu.h
+++ miniKstarMuMu/interface/miniKstarMuMu.h
@@ -22,13 +22,14 @@
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
 #include "DataFormats/PatCandidates/interface/Electron.h"
 #include "DataFormats/PatCandidates/interface/Tau.h"
 #include "DataFormats/PatCandidates/interface/Photon.h"
-#include "DataFormats/PatCandidates/interface/Jet.h"
-#include "DataFormats/PatCandidates/interface/MET.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
+#include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
+
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 
@@ -50,18 +51,24 @@ class miniKstarMuMu : public edm::EDAnalyzer {
       explicit miniKstarMuMu (const edm::ParameterSet&);
       ~miniKstarMuMu();
 
-      std::string getMuCat (reco::Muon const& muon);
+      std::string getMuCat (pat::Muon const& muon);
 
    private:
       virtual void beginJob ();
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
       virtual void endJob ();
+      void         MonteCarloStudies(  const edm::Event& );
+      bool         isAncestor(const reco::Candidate*, const reco::Candidate * );
+      bool         skipOscillations (const reco::GenParticle &, edm::Handle<reco::GenParticleCollection>);
 
       // ----------member data ---------------------------
       edm::EDGetTokenT<reco::VertexCollection>         vtxToken_;
       edm::EDGetTokenT<pat::MuonCollection>            muonToken_;
       edm::EDGetTokenT<pat::PackedCandidateCollection> trackToken_;
       edm::EDGetTokenT<reco::BeamSpot>                 beamSpotToken_;
+
+      edm::EDGetTokenT<reco::GenParticleCollection>         prunedGenToken_;
+      edm::EDGetTokenT<pat::PackedGenParticleCollection>    packedGenToken_;
 
 	  // ####################
 	  // # HLT-trigger cuts #
